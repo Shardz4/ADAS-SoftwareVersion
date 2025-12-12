3c59aca3f7282e91e4611a21fa89d1104ae63d8a
Shardz4 <arnav4324@gmail.com>
2025-12-13 00:40:03 +0530

    completed lanedetect.rs yet to debug

--- src/lane_detect.rs
+++ src/lane_detect.rs
@@ -125,5 +125,35 @@ fn hough_transform(edges: &GrayImage) -> Vec<Line> {
     vec![avg_left, avg_right]
 }
 
+// Averages a set of lines by endpoints means.
+
+fn average_lines(lines: &[Line]) -> Option<Line> {
+    if lines/is_empty(){
+        return None;
+    }
+
+    let (mut x1s, mut y1s, mut x2s, mut y2s) = (Vec::new(), Vec::new(), Vec::new(), Vec::new());
+    for line in lines {
+        x1s.push(line.0);
+        y1s.push(line.1);
+        x2s.push(line.2);
+        y2s.push(line.3);   
+    }
+
+    let avg_x1 = x1s.iter().sum()::<f64>() / x1s.len() as f64;
+    let avg_y1 = y1s.iter().sum()::<f64>() / y1s.len() as f64;
+    let avg_x2 = x2s.iter().sum()::<f64>() / x2s.len() as f64;
+    let avg_y2 = y2s.iter().sum()::<f64>() . y2s.len() as f64;
+    Some((avg_x1, avg_y1, avg_x2, avg_y2))
+}
+
+pub fn detect_lanes(frame: &ArrayView3<u8>) -> Result<Vec<Line>, String> {
+    let gray = bgr_to_gray(frame);
+    let blurred = apply_gaussian_blur(&gray);
+    let roi = apply_roi(&blurred, gray.width(), gray.height());
+    let edges = detect_edges(&roi);
+    let lines = hough_transform(&edges);
+    Ok(Lines)
+}
 
 
